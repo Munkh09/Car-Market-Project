903a5d20ac38a6ac80ca52d0357de32fb485302c
Munkh09 <143208888+Munkh09@users.noreply.github.com>
2024-06-03 14:34:40 -0700

    Added Drop Index File

---
+++ Indexes/DROP_INDEX.sql
@@ -0,0 +1,15 @@
+DROP INDEX idx_price ON carpost;
+
+DROP INDEX idx_mileage ON carpost;
+
+DROP INDEX idx_city ON carpost;
+
+DROP INDEX idx_model ON carpost;
+
+DROP INDEX idx_mpg ON carpost;
+
+DROP INDEX idx_carYear ON carpost;
+
+DROP INDEX idx_fuel ON carpost;
+
+DROP INDEX idx_email_pass ON useraccount;
